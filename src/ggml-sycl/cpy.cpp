6bd94195d46a580b2e80184a618508c6f97c9bf7
Siriuth <104502768+siriuth@users.noreply.github.com>
2026-07-12 07:52:46 +0900

    Refactor cpy_q_q indexing and remove parameters
    Refactor cpy_q_q function to use correct indexing for dimensions and remove unused parameters.

--- src/ggml-sycl/cpy.cpp
+++ src/ggml-sycl/cpy.cpp
@@ -312,7 +312,7 @@ template <typename T, int qk>
 static void cpy_q_q(const char * cx, char * cdst, const int ne, const int ne00, const int ne01, const int ne02,
                       const int nb00, const int nb01, const int nb02, const int nb03, const int ne10, const int ne11,
                       const int ne12, const int nb10, const int nb11, const int nb12, const int nb13,
-                      const int ne000102, const int ne101112,
+//                      const int ne000102, const int ne101112,
                       const sycl::nd_item<3> & item_ct1) {
     const int i = (item_ct1.get_local_range(2) * item_ct1.get_group(2) + item_ct1.get_local_id(2)) * qk;
 
@@ -320,25 +320,25 @@ static void cpy_q_q(const char * cx, char * cdst, const int ne, const int ne00, 
         return;
     }
 
-//    const int i03      = i / (ne00 * ne01 * ne02);
-//    const int i02      = (i - i03 * ne00 * ne01 * ne02) / (ne00 * ne01);
-//    const int i01      = (i - i03 * ne00 * ne01 * ne02 - i02 * ne01 * ne00) / ne00;
-//    const int i00      = i - i03 * ne00 * ne01 * ne02 - i02 * ne01 * ne00 - i01 * ne00;
-    const int i03      = i / (ne000102);
-    const int i02      = (i - i03 * ne000102) / (ne00 * ne01);
-    const int i01      = (i - i03 * ne000102 - i02 * ne01 * ne00) / ne00;
+    const int i03      = i / (ne00 * ne01 * ne02);
+    const int i02      = (i - i03 * ne00 * ne01 * ne02) / (ne00 * ne01);
+    const int i01      = (i - i03 * ne00 * ne01 * ne02 - i02 * ne01 * ne00) / ne00;
     const int i00      = i - i03 * ne00 * ne01 * ne02 - i02 * ne01 * ne00 - i01 * ne00;
+//    const int i03      = i / (ne000102);
+//    const int i02      = (i - i03 * ne000102) / (ne00 * ne01);
+//    const int i01      = (i - i03 * ne000102 - i02 * ne01 * ne00) / ne00;
+//    const int i00      = i - i03 * ne00 * ne01 * ne02 - i02 * ne01 * ne00 - i01 * ne00;
     const int x_offset = (i00 / qk) * nb00 + i01 * nb01 + i02 * nb02 + i03 * nb03;
 
 
-//    const int i13        = i / (ne10 * ne11 * ne12);
-//    const int i12        = (i - i13 * ne10 * ne11 * ne12) / (ne10 * ne11);
-//    const int i11        = (i - i13 * ne10 * ne11 * ne12 - i12 * ne10 * ne11) / ne10;
-//    const int i10        = i - i13 * ne10 * ne11 * ne12 - i12 * ne10 * ne11 - i11 * ne10;
-    const int i13        = i / (ne101112);
-    const int i12        = (i - i13 * ne101112) / (ne10 * ne11);
-    const int i11        = (i - i13 * ne101112 - i12 * ne10 * ne11) / ne10;
+    const int i13        = i / (ne10 * ne11 * ne12);
+    const int i12        = (i - i13 * ne10 * ne11 * ne12) / (ne10 * ne11);
+    const int i11        = (i - i13 * ne10 * ne11 * ne12 - i12 * ne10 * ne11) / ne10;
     const int i10        = i - i13 * ne10 * ne11 * ne12 - i12 * ne10 * ne11 - i11 * ne10;
+//    const int i13        = i / (ne101112);
+//    const int i12        = (i - i13 * ne101112) / (ne10 * ne11);
+//    const int i11        = (i - i13 * ne101112 - i12 * ne10 * ne11) / ne10;
+//    const int i10        = i - i13 * ne10 * ne11 * ne12 - i12 * ne10 * ne11 - i11 * ne10;
     const int dst_offset = (i10 / qk) * nb10 + i11 * nb11 + i12 * nb12 + i13 * nb13;
 
     cpy_blck_q_q<T>(cx + x_offset, cdst + dst_offset);
@@ -1678,7 +1678,7 @@ static void ggml_cpy_q8_0_q8_0(const char * cx, char * cdst, const int ne, const
         sycl::nd_range<3>(sycl::range<3>(1, 1, num_blocks) * sycl::range<3>(1, 1, SYCL_CPY_BLOCK_SIZE),
                               sycl::range<3>(1, 1, SYCL_CPY_BLOCK_SIZE)), [=](sycl::nd_item<3> item_ct1) {
             cpy_q_q<block_q8_0, QK8_0>(cx, cdst, ne, ne00, ne01, ne02, nb00, nb01, nb02, nb03, ne10, ne11, ne12, nb10, nb11, nb12, nb13
-                , ne000102, ne101112
+//                , ne000102, ne101112
                 , item_ct1);
         });
 }
@@ -1697,7 +1697,7 @@ static void ggml_cpy_q5_0_q5_0(const char * cx, char * cdst, const int ne, const
         sycl::nd_range<3>(sycl::range<3>(1, 1, num_blocks) * sycl::range<3>(1, 1, SYCL_CPY_BLOCK_SIZE),
                               sycl::range<3>(1, 1, SYCL_CPY_BLOCK_SIZE)), [=](sycl::nd_item<3> item_ct1) {
             cpy_q_q<block_q5_0, QK5_0>(cx, cdst, ne, ne00, ne01, ne02, nb00, nb01, nb02, nb03, ne10, ne11, ne12, nb10, nb11, nb12, nb13
-                , ne000102, ne101112
+//                , ne000102, ne101112
                 , item_ct1);
         });
 }
@@ -1717,7 +1717,7 @@ static void ggml_cpy_q5_1_q5_1(const char * cx, char * cdst, const int ne, const
         sycl::nd_range<3>(sycl::range<3>(1, 1, num_blocks) * sycl::range<3>(1, 1, SYCL_CPY_BLOCK_SIZE),
                               sycl::range<3>(1, 1, SYCL_CPY_BLOCK_SIZE)), [=](sycl::nd_item<3> item_ct1) {
             cpy_q_q<block_q5_1, QK5_1>(cx, cdst, ne, ne00, ne01, ne02, nb00, nb01, nb02, nb03, ne10, ne11, ne12, nb10, nb11, nb12, nb13
-                , ne000102, ne101112
+//                , ne000102, ne101112
                 , item_ct1);
         });
 }
@@ -1735,7 +1735,7 @@ static void ggml_cpy_q4_0_q4_0(const char * cx, char * cdst, const int ne, const
     stream->parallel_for(
         sycl::nd_range<3>(sycl::range<3>(1, 1, num_blocks) * sycl::range<3>(1, 1, SYCL_CPY_BLOCK_SIZE), sycl::range<3>(1, 1, SYCL_CPY_BLOCK_SIZE)), [=](sycl::nd_item<3> item_ct1) {
             cpy_q_q<block_q4_0, QK4_0>(cx, cdst, ne, ne00, ne01, ne02, nb00, nb01, nb02, nb03, ne10, ne11, ne12, nb10, nb11, nb12, nb13
-                 , ne000102, ne101112
+//                 , ne000102, ne101112
                  , item_ct1);
         });
 }
